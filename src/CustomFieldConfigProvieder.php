d75c4f84389e9e19e7d329ea8d88982ba97c88e1
Anton Pauli <pauli@ironshark.de>
2015-07-30 12:24:25 +0200

    add CustomField model and required traits

---
+++ src/CustomFieldConfigProvieder.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antonpauli
+ * Date: 27/07/15
+ * Time: 18:35
+ */
+
+namespace IronShark\Extendable;
+
+use IronShark\Extendable\CustomFieldType;
+
+class CustomFieldConfigProvieder
+{
+    /**
+     * Return config customfield configs for given model
+     *
+     * @param $modelClass
+     * @return mixed
+     */
+    public static function customFieldConfigs($modelClass){
+        $customFieldsConfig = config('custom-fields');
+        return array_get($customFieldsConfig, $modelClass);
+    }
+
+
+    /**
+     * Return custom field model field name for specified model and fieldtype
+     *
+     * @param $modelClass
+     * @param $fieldName
+     * @return string
+     */
+    public static function fieldType($modelClass, $fieldName){
+        $fieldConfigs = self::customFieldConfigs($modelClass);
+
+        switch ($fieldConfigs[$fieldName]['type']) {
+            case CustomFieldType::Checkbox:
+            case CustomFieldType::Select:
+            case CustomFieldType::String:
+            case CustomFieldType::Radio:
+                return 'stringvalue';
+            case CustomFieldType::Text:
+                return 'textvalue';
+            case CustomFieldType::DateTime:
+                return 'datetime';
+            default:
+                return 'stringvalue';
+        }
+    }
+
+
+    /**
+     * Return all custom field names for specified model
+     *
+     * @param $modelClass
+     * @return array
+     */
+    public static function fieldNames($modelClass) {
+        $customFieldsConfig = config('custom-fields');
+        return array_keys($customFieldsConfig[$modelClass]);
+    }
+}
---
+++ src/CustomFieldType.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antonpauli
+ * Date: 24/07/15
+ * Time: 13:26
+ */
+
+namespace IronShark\Extendable;
+
+
+class CustomFieldType
+{
+    const String = "string";
+    const Text = "text";
+    const Select = "select";
+    const Radio = "radio";
+    const Checkbox = "checkbox";
+    const DateTime = "datetime";
+}
